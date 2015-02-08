7fd4bcf45d305b2a193344c50a65b85cc6ad7908
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-02-08 16:06:44 -0700

    Implement lock and unlock for PetersonTree

--- assignments/hw2/Peterson/PetersonTree.java
+++ assignments/hw2/Peterson/PetersonTree.java
@@ -12,6 +12,7 @@ public class PetersonTree {
 
 		numOfThreads = threads;
 		root = new PetersonNode(null);
+		size++;
 
 		List<PetersonNode> initList = new ArrayList<>();
 		initList.add(root);
@@ -19,12 +20,56 @@ public class PetersonTree {
 		leaves = growLockTree(initList);
 	}
 
-	public List<PetersonNode> growLockTree(List<PetersonNode> nodes) {
-		if (nodes.size() == numOfThreads)
+	// Attempt to lock all nodes starting from the threads corresponding
+	// leaf, all the way to the root of the tree.
+	public void lock(int me) {
+		PetersonNode currentNode = leafLockForThread(me);
+
+		while (currentNode.parent != null) {
+			currentNode.lock(me);
+			currentNode = currentNode.parent;
+		}
+	}
+
+	public void unlock(int me) {
+		PetersonNode currentNode = leafLockForThread(me);
+
+		while (currentNode.parent != null) {
+			currentNode.unlock(me);
+			currentNode = currentNode.parent;
+		}
+	}
+
+	// Helper method which returns the correct leaf node for a thread.
+	private PetersonNode leafLockForThread(int num) {
+
+		// We divide by 2 becauase each lock shares two threads.
+		//   Thread 0 => leafLock 0
+		//   Thread 1 => leafLock 0
+		//   Thread 2 => leafLock 1
+		//   Thread 3 => leafLock 1
+		return leaves.get(num / 2);
+	}
+
+	// Since out number of threads will be a power of two, we can safely
+	// assume that our binary tree will always have leaves such that the
+	// cardinality of the leaves will always be a power of 2. Thus, we grow
+	// the tree row-by-row, per-se. This algorithm recursively fills out
+	// the leaves for a binary tree until the tree has a number of leaves
+	// equal to the number of total threads/2.
+	private List<PetersonNode> growLockTree(List<PetersonNode> nodes) {
+		// Base case:
+		// 	When the number of leaves we have is == 1/2 *
+		// 	number of threads
+		if (nodes.size() == numOfThreads / 2)
 			return nodes;
 
+		// This array will be the current leaves in the iteration for
+		// creating the PetersonTree
 		List<PetersonNode> currentLeaves = new ArrayList<>();
 
+		// Loop through each node, add two childent and store them as
+		// the current leaves
 		for (PetersonNode node : new ArrayList<PetersonNode>(nodes)) {
 			node.leftChild = new PetersonNode(node);
 			node.rightChild = new PetersonNode(node);
@@ -32,15 +77,16 @@ public class PetersonTree {
 			currentLeaves.add(node.leftChild);
 			currentLeaves.add(node.rightChild);
 
-			size +=2;
+			size += 2;
 		}
 
+		// Recurse, passing our current leaves back to the function
 		return growLockTree(currentLeaves);
 	}
 
 	@Override public String toString() {
 		return    "TotalSize: " + size + "\n"
-	       		+ "Leaves size: " + leaves.size() + "\n";
+	       		+ "Leaves size: " + leaves.size();
 	}
 
 	private void validateArgs(int threads) {
