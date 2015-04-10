521242611ed05922ded002aad1c0922cfad42809
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-04-10 13:08:54 -0600

    Add sequentual queue

---
+++ assignments/hw4/Problem2/BroadcastingConcurrentQueue.java
@@ -0,0 +1,92 @@
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.List;
+import java.util.Arrays;
+
+public class BroadcastingConcurrentQueue<T> {
+	private final Node head, tail;
+	private final ReentrantLock lock = new ReentrantLock();
+	private volatile int size;
+
+	private class Node {
+		public T item;
+		public Node next;
+		public Node prev;
+
+		public Node(T data) {
+			item = data;
+		}
+	}
+
+	public BroadcastingConcurrentQueue() {
+		head = new Node(null);
+		tail = new Node(null);
+
+		head.prev = tail;
+		tail.next = head;
+
+		size = 0;
+	}
+
+	public T push(T item) {
+		Node newNode = new Node(item);
+		newNode.next = tail.next;
+
+		if (isEmpty()) head.prev = newNode;
+
+		tail.next.prev = newNode;
+		tail.next = newNode;
+
+		size++;
+
+		return item;
+	}
+
+	public T pop() {
+		if (isEmpty()) return null;
+
+		T poppedData = head.prev.item;
+		head.prev = head.prev.prev;
+
+		size--;
+
+		return poppedData;
+	}
+
+	public boolean contains(T item) {
+		Node current = tail.next;
+
+		for (int i = 0; i < size; i++) {
+			if (current.item == item) return true;
+			current = current.next;
+		}
+
+		return false;
+	}
+
+	private boolean isEmpty() {
+		return size == 0;
+	}
+
+	public static void main(String[] args) {
+		BroadcastingConcurrentQueue<String> queue;
+		queue = new BroadcastingConcurrentQueue<String>();
+
+		List<String> foods = Arrays.asList(
+			"Cheerios",
+			"Feta cheese",
+			"Salmon smoothie"
+		);
+
+		for (String food : foods)
+			queue.push(food);
+
+		System.out.println(queue.contains("Cheerios"));
+		System.out.println(queue.pop());
+		System.out.println(queue.contains("Cheerios"));
+
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+		System.out.println(queue.pop());
+
+	}
+}
