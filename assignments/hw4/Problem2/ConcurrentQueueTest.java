786f66761c186b2e7d95daafa71c2bba2bb2fcc4
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-04-14 11:57:52 -0600

    Tag the test threads

--- assignments/hw4/Problem2/ConcurrentQueueTest.java
+++ assignments/hw4/Problem2/ConcurrentQueueTest.java
@@ -34,6 +34,8 @@ public class ConcurrentQueueTest implements Runnable {
     private static void pass(String msg) {
         String ANSI_GREEN = "\u001B[32m";
         String ANSI_RESET = "\u001B[0m";
+        long id = Thread.currentThread().getId();
+        msg = "Thread " + id + ": " + msg;
 
         System.out.println(ANSI_GREEN + msg + ANSI_RESET);
     }
@@ -41,6 +43,8 @@ public class ConcurrentQueueTest implements Runnable {
     private static void fail(String msg) {
         String ANSI_RED = "\u001B[31m";
         String ANSI_RESET = "\u001B[0m";
+        long id = Thread.currentThread().getId();
+        msg = "Thread " + id + ": " + msg;
 
         System.out.println(ANSI_RED + msg + ANSI_RESET);
     }
@@ -63,6 +67,7 @@ public class ConcurrentQueueTest implements Runnable {
     }
 
     public static void main(String[] args) {
+        System.out.println("Starting tests for Broadcaster.java...");
         q = new Broadcaster<String>(threadCount);
         spawnThreads(q);
     }
